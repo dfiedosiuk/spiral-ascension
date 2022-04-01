fd4e2776d4a892307e2c5d67b6b55cc3fd458bba
Daniel <d.fiedosiuk@adform.com>
2022-04-01 18:07:24 +0200

    init commit

---
+++ src/main/scala/Spiral.scala
@@ -0,0 +1,3 @@
+object Spiral extends App {
+
+}
---
+++ src/test/scala/SpiralSpec.scala
@@ -0,0 +1,14 @@
+import org.scalatest._
+import flatspec._
+import matchers._
+
+
+class SpiralSpec extends AnyFlatSpec with should.Matchers {
+
+  behavior of "A Spiral.create()"
+
+  it should "return " in {
+    create() = ???
+  }
+
+}
